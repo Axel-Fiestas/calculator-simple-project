2e76c3bd1e230441624e9360d582d1ffaf876bfa
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2024-01-22 23:42:59 -0500

    feat: basic html estructure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Calculator</h1>
+      <Calculator />
     </div>
   );
 }
 
+function Calculator() {
+  return (
+    <div>
+      <CurrentNumber />
+      <KeyboardPad />
+    </div>
+  );
+}
+
+function CurrentNumber() {
+  const number = useState(0);
+
+  return <h1>{number}</h1>;
+}
+
+function KeyboardPad() {
+  return (
+    <div className="pad">
+      <div className="pad_row">
+        <button>%</button>
+        <button>CE</button>
+        <button>C</button>
+        <button>->X</button>
+      </div>
+
+      <div className="pad_row">
+        <button>1/x</button>
+        <button>x^2</button>
+        <button>Raiz X</button>
+        <button>/</button>
+      </div>
+
+      <div className="pad_row">
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>x</button>
+      </div>
+
+      <div className="pad_row">
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+      </div>
+
+      <div className="pad_row">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+      </div>
+
+      <div className="pad_row">
+        <button>+/-</button>
+        <button>0</button>
+        <button>.</button>
+        <button>=</button>
+      </div>
+    </div>
+  );
+}
 export default App;
